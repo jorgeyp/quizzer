c385e34e7c65a7a29c95e353f728a5d6203ce0d4
Jorge Yagüe París <jorgeyp@gmail.com>
2014-10-11 15:53:31 +0200

    add parse JSON documents form manifest

--- lib/quizzer/options.rb
+++ lib/quizzer/options.rb
@@ -4,17 +4,40 @@ module Quizzer
 
   class Options
 
+    DEFAULT_MANIFEST = 'manifest.json'
+
+    attr_reader :manifest
+
+    def initialize(argv)
+      @manifest = DEFAULT_MANIFEST
+      parse(argv)
+    end
+
     private
 
     def parse(argv)
       OptionParser.new do |opts|
         opts.banner = 'Usage: quizzer [options] quizz_file asessment_file'
 
+        opts.on('-m', '--manifest manifest', String, 'Get JSON files URIs from manifest file.') do |m|
+          @manifest = m
+        end
+
+        opts.on_tail('-h', '--help', 'Show this message') do
+          puts opts
+          exit
+        end
+
+        # opts.on_tail("-v", "--version", "Show version") do
+        #   puts ::Version.join('.')
+        #   exit
+        # end
+
         begin
           opts.parse!(argv)
-          rescue OptionParser::ParseError => e
-            STDERR.puts e.message, "\n", opts
-            exit(-1)
+        rescue OptionParser::ParseError => e
+          STDERR.puts e.message, "\n", opts
+          exit(-1)
         end
       end
     end # parse()
--- lib/quizzer/quizzer.rb
+++ lib/quizzer/quizzer.rb
@@ -5,18 +5,44 @@ require 'open-uri'
 module Quizzer
 
   class Quizzer
+
+    def initialize(assessment, quizz, scores)
+      @assessment = assessment
+      @quizz = quizz
+      @scores = scores
+
+      p assessment
+      p quizz
+      p scores
+    end
+
+
+  end
+
+  class Main
+
     def initialize(argv)
       @options = Options.new(argv)
-      @quizz
-      @assesment
-      @scores
+    end
+
+    def run
+      if @options.manifest
+        manifest = parse_json(@options.manifest)
+
+        manifest["tests"].each { |test|
+          assesment = parse_json(test["assessment"])
+          quizz = parse_json(test["quizz"])
+          scores = parse_json(test["scores"])
+
+          @quizzer = Quizzer.new(assesment, quizz, scores)
+        }
+      end
+
     end
 
     def parse_json(path)
-      JSON.parse(IO.read(path))
+      JSON.load(open(path))
+      #JSON.parse(IO.read(path))
     end
   end
 end
-
-quizzer = Quizzer.new
-quizzer.parse_json('manifest.json')
